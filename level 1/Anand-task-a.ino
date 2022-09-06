cc0a3a3a88e8451d818fe0334e6fbdfddae1dd1f
Anand S <anandzzz360@gmail.com>
2022-09-06 12:08:19 +0530

    Task-a

---
+++ level 1/Anand-task-a.ino
@@ -0,0 +1,19 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // Repeatedly blink 4 leds:
+  int i = 2;  //Initial led pin
+  for(i=2;i<=5;i++){    //Leds from 2 to 5 pins
+    digitalWrite(i, HIGH);    //Led high
+    delay(250);   //delay
+  }
+  delay(1000);
+  for(i=2;i<=5;i++){
+    digitalWrite(i, LOW);   //Led low
+    delay(250);
+  }
+  delay(1000);
+}
